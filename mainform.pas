977be3f6304f8708c65a50c261910e2691902e6f
Kristian Bunovski <kristiyan@wikieye.com>
2018-01-27 15:20:26 +0200

    added JPEG compression and default name for final image

--- mainform.pas
+++ mainform.pas
@@ -27,6 +27,7 @@ type
     LeftPicture: TPicture;
     RightPicture: TPicture;
     IsLoaded: byte;
+    CombinedFileName: string;
   public
 
   end;
@@ -41,6 +42,8 @@ implementation
 { TForm1 }
 
 procedure TForm1.OpenLeftPictureClick(Sender: TObject);
+var
+  Filename: string;
 begin
   if BrowsePicture.Execute then
   begin
@@ -48,6 +51,9 @@ begin
     begin
       LeftPicture.LoadFromFile(BrowsePicture.FileName);
       IsLoaded := IsLoaded or $0F;
+      Filename := ExtractFileName(BrowsePicture.FileName);
+      Filename := Copy(Filename, 1, Filename.LastIndexOf('.'));
+      CombinedFileName := Filename + '_' + CombinedFileName;
     end;
   end;
 
@@ -57,6 +63,8 @@ begin
 end;
 
 procedure TForm1.OpenRightPictureClick(Sender: TObject);
+var
+  Filename: string;
 begin
   if BrowsePicture.Execute then
   begin
@@ -64,6 +72,9 @@ begin
     begin
       RightPicture.LoadFromFile(BrowsePicture.FileName);
       IsLoaded := IsLoaded or $F0;
+      Filename := ExtractFileName(BrowsePicture.FileName);
+      Filename := Copy(Filename, 1, Filename.LastIndexOf('.'));
+      CombinedFileName := CombinedFileName + Filename;
     end;
 
     if IsLoaded = $FF then
@@ -76,28 +87,41 @@ begin
   LeftPicture := TPicture.Create;
   RightPicture := TPicture.Create;
   IsLoaded := 0;
+  CombinedFileName := '';
 end;
 
 procedure TForm1.CombineClick(Sender: TObject);
 var
   CombinedResult: TBitmap;
+  JPEG: TJPEGImage;
 begin
   CombinedResult := TBitmap.Create;
+  JPEG := TJPEGImage.Create;
   try
     CombinedResult.Width := LeftPicture.Width + RightPicture.Width;
     CombinedResult.Height := Math.Max(LeftPicture.Height, RightPicture.Height);
     CombinedResult.Canvas.Draw(0, 0, LeftPicture.Graphic);
     CombinedResult.Canvas.Draw(LeftPicture.Width, 0, RightPicture.Graphic);
 
+    JPEG.Assign(CombinedResult);
+    JPEG.CompressionQuality := 80;
+
+    SavePictureDialog.FileName := CombinedFileName;
+
     if SavePictureDialog.Execute then
     begin
-      CombinedResult.SaveToFile(SavePictureDialog.FileName);
+      JPEG.SaveToFile(SavePictureDialog.FileName);
       IsLoaded := 0;
       Combine.Enabled := False;
+      CombinedFileName := '';
     end;
 
   finally
+    IsLoaded := 0;
+    Combine.Enabled := False;
+    CombinedFileName := '';
     CombinedResult.Free;
+    JPEG.Free;
   end;
 end;
 
